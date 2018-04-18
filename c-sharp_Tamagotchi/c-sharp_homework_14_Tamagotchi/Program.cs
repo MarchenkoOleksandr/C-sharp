b8409a2dc299008bad14113b92154f745b584998
Aleksandr <38476175+MarchenkoOleksandr@users.noreply.github.com>
2018-04-19 00:29:36 +0300

    Tamagotchi game (light version)

---
+++ c-sharp_Tamagotchi/c-sharp_homework_14_Tamagotchi/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using System.Timers;
+using static System.Console;
+
+namespace Tamagotchi
+{
+    class Program
+    {
+        public static System.Timers.Timer timer = new System.Timers.Timer { Interval = 2000 };
+        public static System.Timers.Timer totalTime = new System.Timers.Timer { Interval = 60000 };
+
+        static void Main(string[] args)
+        {
+            OutputEncoding = Encoding.Unicode;
+            InputEncoding = Encoding.Unicode;
+            Title = "Tamagotchi";
+
+            WriteLine("Вітаємо! Ця програма імітує роботу Тамагочі!\n");
+            
+            Tamagotchi tamagotchi = new Tamagotchi();
+            Action action = new Action(tamagotchi.NewAction());
+            WriteLine("\nПривіт! Давай дружити!");
+            tamagotchi.Show();
+            timer.Start();
+            totalTime.Start();
+            timer.Elapsed += (sender, e) => OnTimer(action, e, tamagotchi);
+            totalTime.Elapsed += (sender, e) => TotalTimer(tamagotchi, e);
+            timer.AutoReset = true;
+            totalTime.AutoReset = false;
+
+            ReadKey(true);
+        }
+
+        public static void OnTimer(Action action, ElapsedEventArgs e, Tamagotchi tamagotchi)
+        {
+            action = new Action(tamagotchi.NewAction());
+            timer.Stop();
+            if (MessageBox.Show($"{action.Invoke()}", "Нове прохання", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1) == DialogResult.No)
+            {
+                WriteLine("\nЯ засмучений!");
+                tamagotchi.Failures++;
+            }
+            else
+            {
+                WriteLine("\nДякую!");
+                if (tamagotchi.Failures > 0)
+                    tamagotchi.Failures--;
+            }
+
+            if (tamagotchi.Failures == 3 && MessageBox.Show($"{tamagotchi.NeedTreatment()}", "Нове прохання", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1) == DialogResult.Yes)
+            {
+                WriteLine("\n\nДякую!");
+                tamagotchi.Failures = 0;
+            }
+            tamagotchi.Show();
+            if (timer.AutoReset == true)
+                timer.Start();
+
+            if (tamagotchi.Failures == 3)
+                End(tamagotchi.Failures);
+        }
+
+        public static void TotalTimer(Tamagotchi tamagotchi, ElapsedEventArgs e)
+        {
+            ForegroundColor = ConsoleColor.DarkRed;
+            WriteLine("\nЧас гри закінчився!");
+            ResetColor();
+            timer.AutoReset = false;
+            End(tamagotchi.Failures);
+        }
+
+        public static void End(int i)
+        {
+            if (i == 3)
+            {
+                ForegroundColor = ConsoleColor.DarkRed;
+                WriteLine("\nЯ ображений! Прощавай!");
+            }
+            else
+            {
+                ForegroundColor = ConsoleColor.DarkGreen;
+                WriteLine("\nМи стали справжніми друзями!");
+            }
+            ResetColor();
+            timer.Stop();
+        }
+    }
+}
---
+++ c-sharp_Tamagotchi/c-sharp_homework_14_Tamagotchi/Tamagotchi.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using static System.Console;
+
+namespace Tamagotchi
+{
+    public delegate string Action();
+
+    class Tamagotchi
+    {
+        int choise = new Random().Next(0, 4);
+        public int Failures { get; set; } = 0;
+
+        public string WantEat() { return "Хочу їсти! Погодуєш мене?"; }
+
+        public string WantWalk() { return "Хочу гуляти! Вигуляєш мене?"; }
+
+        public string WantSleep() { return "Хочу спати! Вкладеш мене спати?"; }
+
+        public string NeedTreatment() { return "Я хворію! Полікуєш мене?"; }
+
+        public string WantPlay() { return "Хочу грати! Пограєш зі мною?"; }
+
+        public Action NewAction()
+        {
+            switch ((choise++) % 5)
+            {
+                case 0:
+                    return WantEat;
+                case 1:
+                    return WantWalk;
+                case 2:
+                    return WantSleep;
+                case 3:
+                    return NeedTreatment;
+                default:
+                    return WantPlay;
+            }
+        }
+
+        public void Show()
+        {
+            switch (Failures)
+            {
+                case 0:
+                    ForegroundColor = ConsoleColor.DarkGreen;
+                    WriteLine("\n  ___\n /. .\\\n|  _  |\n \\___/\n /   \\\n");
+                    ResetColor();
+                    break;
+                case 1:
+                    ForegroundColor = ConsoleColor.DarkYellow;
+                    WriteLine("\n  ___\n /. .\\\n|  o  |\n \\___/\n /   \\\n");
+                    ResetColor();
+                    break;
+                default:
+                    ForegroundColor = ConsoleColor.DarkRed;
+                    WriteLine("\n  ___\n /. .\\\n|  ~  |\n \\___/\n /   \\\n");
+                    ResetColor();
+                    break;
+            }
+        }
+    }
+}
