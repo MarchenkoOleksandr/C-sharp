363ea26e8efb725e5b2b3e19ef8ce49e93912173
Aleksandr <38476175+MarchenkoOleksandr@users.noreply.github.com>
2018-04-19 00:27:52 +0300

    Parser of the site finance.i.ua (USD to XML file)

---
+++ c-sharp_finance/c-sharp_lab_12_finance/Bank.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using static System.Console;
+
+namespace finance
+{
+    class Bank
+    {
+        public string BankName { get; set; }
+        public string USDbuy { get; set; }
+        public string USDsale { get; set; }
+        
+        public override string ToString()
+        {
+            return $"{BankName.PadRight(30)} {USDbuy}     {USDsale}";
+        }
+
+        public static void Save(List<Bank> banks)
+        {
+            try
+            {
+                using (XmlTextWriter writer = new XmlTextWriter($"output.xml", Encoding.Unicode))
+                {
+                    writer.Formatting = Formatting.Indented;
+                    writer.WriteStartDocument();
+                    writer.WriteStartElement("banks");
+                    foreach (Bank item in banks)
+                    {
+                        writer.WriteStartElement("bank");
+                        writer.WriteElementString("BankName", item.BankName);
+                        writer.WriteElementString("USDbuy", item.USDbuy);
+                        writer.WriteElementString("USDsale", item.USDsale);
+
+                        writer.WriteEndElement();
+                    }
+                    writer.WriteEndElement();
+                    writer.WriteEndDocument();
+                }
+                ForegroundColor = ConsoleColor.DarkGreen;
+                WriteLine($"\n\nДані збережені у файлі \"output.xml\"!");
+            }
+            catch (Exception e)
+            {
+                ForegroundColor = ConsoleColor.DarkRed;
+                WriteLine(e.Message);
+            }
+            ResetColor();
+        }
+    }
+}
---
+++ c-sharp_finance/c-sharp_lab_12_finance/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Text.RegularExpressions;
+using static System.Console;
+
+namespace finance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OutputEncoding = Encoding.Unicode;
+            InputEncoding = Encoding.Unicode;
+            Title = "Get exchange rates";
+
+            WriteLine("Вітаємо! Це програма-парсер, яка виводить курс долара з сайту finance.i.ua\n");
+
+            WebClient client = new WebClient { Encoding = Encoding.UTF8 };
+            string link = "https://finance.i.ua/";
+            string siteText = client.DownloadString(link);
+
+            Regex regex = new Regex("<a href=\"/bank/\\d+/\">[\\w\\s-]*</a><time></time></th><td class=\"buy_rate\"><span class=\"[\\w\\s-]*\"><span>\\d+\\.\\d+</span></span></td><td class=\"sell_rate\"><span class=\"[\\w\\s-]*\"><span>\\d+\\.\\d+</span>");
+            MatchCollection matches = regex.Matches(siteText.Remove(siteText.LastIndexOf("bank_rates_eur")));
+
+            if (matches.Count > 0)
+            {
+                string[] bankName = new string[matches.Count];
+                List<Bank> banks = new List<Bank>(matches.Count);
+                foreach (Match match in matches)
+                {
+                    banks.Add(new Bank());
+                    banks[banks.Count - 1].BankName = Regex.Replace(Regex.Replace(match.Value, "<a href=\"/bank/\\d+/\">", String.Empty), "</a><time></time></th><td class=\"buy_rate\"><span class=\"[\\w\\s-]*\"><span>\\d+\\.\\d+</span></span></td><td class=\"sell_rate\"><span class=\"[\\w\\s-]*\"><span>\\d+\\.\\d+</span>", String.Empty);
+                    banks[banks.Count - 1].USDbuy = Regex.Replace(Regex.Replace(match.Value, "<a href=\"/bank/\\d+/\">[\\w\\s-]*</a><time></time></th><td class=\"buy_rate\"><span class=\"[\\w\\s-]*\"><span>", String.Empty), "</span></span></td><td class=\"sell_rate\"><span class=\"[\\w\\s-]*\"><span>\\d+\\.\\d+</span>", String.Empty);
+                    banks[banks.Count - 1].USDsale = Regex.Replace(Regex.Replace(match.Value, "<a href=\"/bank/\\d+/\">[\\w\\s-]*</a><time></time></th><td class=\"buy_rate\"><span class=\"[\\w\\s-]*\"><span>\\d+\\.\\d+</span></span></td><td class=\"sell_rate\"><span class=\"[\\w\\s-]*\"><span>", String.Empty), "</span>", String.Empty);
+                }
+                ForegroundColor = ConsoleColor.DarkGreen;
+                WriteLine($"{"Назва банку".PadRight(30)} покупка/продаж (долар)");
+                ResetColor();
+                foreach (Bank item in banks)
+                    WriteLine(item);
+                Bank.Save(banks);
+            }
+            else
+            {
+                ForegroundColor = ConsoleColor.DarkRed;
+                WriteLine($"\nПомилка отримання даних з сайту finance.i.ua!");
+                ResetColor();
+            }
+            ReadKey(true);
+        }
+    }
+}
